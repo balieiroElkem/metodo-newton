4f5074a69f00e46295136b5d4b95bcaf987b4e17
Elkem Balieiro <elkem.balieiroo@gmail.com>
2023-09-28 20:54:48 -0300

    Tirando os comentários

--- js/funcoes.js
+++ js/funcoes.js
@@ -20,24 +20,6 @@ function calcula(equacao1, equacao2, pontx1, pontx2) {
     const resultMultiply = math.multiply(math.inv(matrizJacobiana), matrizEquacao);
     const resultx1 = math.add(math.matrix([[pontx1],[pontx2]]), resultMultiply);
     return resultx1;
-    // let x = math.clone(x0);
-    // let iteracoes = 0;
-
-    // while (iteracoes < maxIter) {
-    //     let fx = f.evaluate({ x: x });
-    //     let jacobian = calcularJacobian(df, x);
-    //     let delta = math.lusolve(jacobian, math.multiply(-1, fx)).valueOf();
-
-    //     x = math.add(x, delta);
-
-    //     if (normaEuclidiana(delta) < epsilon) {
-    //         return x;
-    //     }
-
-    //     iteracoes++;
-    // }
-
-    // throw new Error('Não foi possível encontrar uma solução dentro do número máximo de iterações.');
 }
 
 function normaEuclidiana(vetor) {
@@ -53,16 +35,6 @@ function jacobiana(equacao1, equacao2, variavel1, variavel2, pontx1, pontx2) {
         [derivada(equacao2, variavel1).evaluate(obj), derivada(equacao2, variavel2).evaluate(obj)]
     ]);
     return jacobiana;
-
-    // let jacobian = math.zeros(x.length, x.length);
-
-    // for (let i = 0; i < x.length; i++) {
-    //     for (let j = 0; j < x.length; j++) {
-    //         jacobian._data[i][j] = df[i].evaluate({ x: x });
-    //     }
-    // }
-
-    // return jacobian;
 }
 
 function formatMatrizEquacao(equacao1, equacao2, variavel1, variavel2, pontx1, pontx2) {
